df2b96324f1a6b721f5eef1fed561efbba8f3218
Ayrus16 <suryap.sp83@gmail.com>
2022-10-21 13:57:31 +0700

    Menyelesaikan Video #3 - Struktur folder, routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "Surya Putra Pratama",
+        "email" => "203040065@unpas.ac.id",
+        "image" => "nophoto.jpg"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('post');
 });
