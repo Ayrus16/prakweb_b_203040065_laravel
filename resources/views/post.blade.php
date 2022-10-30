5d1e80ff7652828217af71ccc71be6ae297dee4a
Ayrus16 <suryap.sp83@gmail.com>
2022-10-30 11:31:52 +0700

    Menyelesaikan Video #8 Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,6 +5,8 @@
 
     <article class="mb-5">
         <h2>{{ $post->title }}</h2>
+        <p>By. Surya Putra in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}
+        </a></p>
         {!! $post->body !!}
     </article>  
     
